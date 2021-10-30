dd6e6b52442b683a95ad3c6b19bfb16725de394b
mahjabeen-a <mahjabeen.aka@gmail.com>
2021-10-30 21:30:10 +0530

    added product details

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
+  products: any[];
+  constructor() {
+    this.products = [ {id: 1, name: "One plus nord", price: 25000, description: "Triple rear camera", imagepath: "./assets/images/oneplus.jpg"}, 
+                      {id: 2, name: "Redmi Note 10 pro", price: 18000, description: "Quad camera", imagepath: "./assets/images/redmi.jpg"},
+                      {id: 3, name: "Samsung Galaxy", price: 10000, description: "Quad camera", imagepath: "./assets/images/samsung.jpg"}
+                    ]
+  }
 
   ngOnInit(): void {
   }
